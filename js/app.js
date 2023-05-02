901e276de2792eb15310b858eb85b6caa95aa371
Nicolás Caminos <nicolas_caminos@outlook.com>
2023-05-02 18:23:04 -0300

    update

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const preferedColorScheme = window.matchMedia('(prefers-color-scheme: light)').matches ? 'dark' : 'light';
+const preferedColorScheme = window.matchMedia('(prefers-color-scheme: light)').matches ? 'light' : 'dark';
 const slider = document.getElementById('slider');
 
 const setTheme = (theme) => {
