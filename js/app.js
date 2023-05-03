80ee1115c952cd99422eefefa27d67af0e0cb20e
Nicolás Caminos <nicolas_caminos@outlook.com>
2023-05-03 11:34:33 -0300

    local

--- js/app.js
+++ js/app.js
@@ -1,28 +1,4 @@
 
-// Escuchar cambios en checkbox
-const preferedColorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'light' : 'dark';
-const slider = document.getElementById('slider');
-
-const setTheme = (theme) => {
-    document.documentElement.setAttribute('data-theme', theme);
-    localStorage.setItem('theme', theme);
-}
-
-slider.addEventListener('click', () => {
-    let switchToTheme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark';
-    setTheme(switchToTheme);
-});
-
-// Buscar valor en localStorage, si no existe, poner en 'off'
-let DarkMode = localStorage.getItem('dark') || 'on';
-// Obtener el checkbox
-let checkDark = document.getElementById('slider');
-// Marcar checkbox y aplicar estilo a body si darkMode = 'on'
-if (DarkMode == 'on') {
-    checkDark.checked = true;
-}
-
-setTheme(localStorage.getItem('theme') || preferedColorScheme);
 
 
 // Example starter JavaScript for disabling form submissions if there are invalid fields
