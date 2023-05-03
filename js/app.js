a06c486ba2d4b5502525cfe5b2f68a31146c1789
Nicolás Caminos <nicolas_caminos@outlook.com>
2023-05-03 10:00:13 -0300

    preferedColorScheme

--- js/app.js
+++ js/app.js
@@ -23,28 +23,29 @@ setTheme(localStorage.getItem('theme') || preferedColorScheme);
 
 function check() {
     const slider = document.getElementById('slider');
-    if (localStorage.getItem('theme') === 'dark'()) {
+    if (preferedColorScheme === 'dark'()) {
 
         $("#slider").click();
 
     }
+}
 
-    // Example starter JavaScript for disabling form submissions if there are invalid fields
-    (() => {
-        'use strict'
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
 
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        const forms = document.querySelectorAll('.needs-validation')
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
 
-        // Loop over them and prevent submission
-        Array.from(forms).forEach(form => {
-            form.addEventListener('submit', event => {
-                if (!form.checkValidity()) {
-                    event.preventDefault()
-                    event.stopPropagation()
-                }
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
 
-                form.classList.add('was-validated')
-            }, false)
-        })
-    })()
+            form.classList.add('was-validated')
+        }, false)
+    })
+})()
