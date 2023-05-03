24e87a8d720b439fa4eb79dbf76c9b0e5f0870ea
Nicolás Maximiliano Caminos <45422512+NicolasCaminos@users.noreply.github.com>
2023-05-03 17:29:25 -0300

    Actualizar app.js

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ slider.addEventListener('click', () => {
 });
 
 // Buscar valor en localStorage, si no existe, poner en 'off'
-let DarkMode = localStorage.getItem('dark') || 'on';
+let DarkMode = localStorage.getItem('dark') || 'off';
 //Obtener el checkbox
 let checkDark = document.getElementById('slider');
 // Marcar checkbox y aplicar estilo a body si darkMode = 'off'
