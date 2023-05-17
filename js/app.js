436ed2435e85cd2d306f38466add0116afe2b681
Nicolás Caminos <nicolas_caminos@outlook.com>
2023-05-17 20:50:06 -0300

    galery

--- js/app.js
+++ js/app.js
@@ -61,3 +61,13 @@ checkDark.addEventListener('change', function () {
             }, false)
         })
     })()
+//Animation
+function pageLoaded() {
+    let loaderSection = document.querySelector('.loader-section');
+    loaderSection.classList.add('loaded');
+}
+
+document.onload = pageLoaded();
+function pageLoaded() {
+    let loaderSection = document.querySelector('.loader-section'); loaderSection.classList.add('loaded');
+}
