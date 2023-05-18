fe026b90df7b51f8120ebaff4dc1d7545299e3cd
Nicolás Caminos <nicolas_caminos@outlook.com>
2023-05-17 21:53:02 -0300

    countries

--- js/app.js
+++ js/app.js
@@ -18,12 +18,12 @@ setTheme(localStorage.getItem('theme') || preferedColorScheme);
 //JS Boostrap
 document.addEventListener('DOMContentLoaded', () => {
     // Buscar valor en localStorage, si no existe, poner en 'off'
-    let darkMode = localStorage.getItem('dark') || 'on';
+    let darkMode = localStorage.getItem('dark') || 'off';
     // Obtener el checkbox
     let checkDark = document.getElementById('slider');
-    // Marcar checkbox y aplicar estilo a body si darkMode = 'on'
+    // Marcar checkbox y aplicar estilo a body si darkMode = 'off'
     if (darkMode == 'on') {
-        slider.checked = true;
+        checkDark.checked = true;
     }
     // Escuchar cambios en checkbox
     checkDark.addEventListener('change', e => {
@@ -41,33 +41,3 @@ checkDark.addEventListener('change', function () {
     window.localStorage.setItem(slider.id, slider.checked)
 })
 
-    //JS Boostrap
-    // Example starter JavaScript for disabling form submissions if there are invalid fields
-    (() => {
-        'use strict'
-
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        const forms = document.querySelectorAll('.needs-validation')
-
-        // Loop over them and prevent submission
-        Array.from(forms).forEach(form => {
-            form.addEventListener('submit', event => {
-                if (!form.checkValidity()) {
-                    event.preventDefault()
-                    event.stopPropagation()
-                }
-
-                form.classList.add('was-validated')
-            }, false)
-        })
-    })()
-//Animation
-function pageLoaded() {
-    let loaderSection = document.querySelector('.loader-section');
-    loaderSection.classList.add('loaded');
-}
-
-document.onload = pageLoaded();
-function pageLoaded() {
-    let loaderSection = document.querySelector('.loader-section'); loaderSection.classList.add('loaded');
-}
